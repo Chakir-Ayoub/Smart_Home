e5382bd2b62df00a7b14ba97b2ef6ff78231cbb3
Chakir Ayoub <ayoub.chakir0926@gmail.com>
2023-12-03 12:21:13 +0100

    First push

---
+++ src/test/java/com/smart/home/SmartHomeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.smart.home;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SmartHomeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
